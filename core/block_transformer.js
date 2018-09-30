b20635d5fb773b06e03f78b1dcef17694196bc08
Peeratham Techapalokul <tpeera4@vt.edu>
2018-09-29 23:49:43 -0400

    grouping of refactoring transformations as single event

--- core/block_transformer.js
+++ core/block_transformer.js
@@ -6,10 +6,17 @@ Blockly.BlockTransformer = function (workspace) {
 
 
 Blockly.BlockTransformer.prototype.doTransform = function (refactorable) {
-    console.log(refactorable);
-    for (var action of refactorable.transforms) {
-        this.apply(action);
+    // Fire a create event.
+      Blockly.Events.setGroup(true);
+    try {
+        console.log(refactorable);
+        for (var action of refactorable.transforms) {
+            this.apply(action);
+        }
+    } finally {
+        Blockly.Events.setGroup(false);
     }
+    
 };
 
 Blockly.BlockTransformer.prototype.apply = function (action) {
@@ -57,62 +64,62 @@ Blockly.BlockTransformer.prototype.InsertBlockAction = function (action) {
     return true;
 };
 
-Blockly.BlockTransformer.prototype.ReplaceAction = function(action){
+Blockly.BlockTransformer.prototype.ReplaceAction = function (action) {
     var targetBlock = this.workspace.getBlockById(action.target_block);
     var targetBlockList = action.targetBlockList;
     var replaceWith = this.workspace.getBlockById(action.replace_with);
     // if(!targetBlock){
     // 	return;
     // }
-    if(targetBlock){
-      var parentConnection = targetBlock.outputConnection.targetConnection;
-      targetBlock.unplug(true,true);
-      parentConnection.connect(replaceWith.outputConnection);
-      // delete old value
-      targetBlock.dispose();
+    if (targetBlock) {
+        var parentConnection = targetBlock.outputConnection.targetConnection;
+        targetBlock.unplug(true, true);
+        parentConnection.connect(replaceWith.outputConnection);
+        // delete old value
+        targetBlock.dispose();
     }
-  
+
     //todo handle when targetBlockList has one block to replace (e.g. if block)
-    if(targetBlockList){
-      var targetBlocks = [];
-      for( var blockID of targetBlockList){
-        targetBlocks.push(this.workspace.getBlockById(blockID));
-      }
-  
-      //get previous conn of top most block
-      var topmostBlock = targetBlocks[0];
-  
-      let targetBlock = topmostBlock;
-      let insertedBlock = replaceWith;
-      
-      //handle the case when there's a block connected to the block target
-      // let previousBlock = targetBlock.previousConnection.targetBlock();
-      let prevConn = targetBlock.previousConnection.targetConnection;
-      if(prevConn){
-        prevConn.connect(insertedBlock.previousConnection);
-      }else{
-        // place inserted block close to the postion of the target block to avoid block jump
-        let xy = targetBlock.getRelativeToSurfaceXY();
-        insertedBlock.moveBy(xy.x, xy.y);
-      }
-  
-      // unplug bottom block
-      let lastBlock = targetBlocks[targetBlocks.length-1];
-      
-      // the block after the replaced fragment
-      let nextBlock = lastBlock.nextConnection.targetBlock();
-      if(nextBlock){
-        let nextConn = lastBlock.nextConnection.targetBlock().previousConnection;
-        lastBlock.unplug(); //must be unplugged before nextConn can connect to the inserted block
-        insertedBlock.nextConnection.connect(nextConn);
-      }
-  
-      // dispose the fragment
-      while(targetBlocks.length>0){
-        var toDispose = targetBlocks.pop();
-        toDispose.dispose();
-      }
-  
+    if (targetBlockList) {
+        var targetBlocks = [];
+        for (var blockID of targetBlockList) {
+            targetBlocks.push(this.workspace.getBlockById(blockID));
+        }
+
+        //get previous conn of top most block
+        var topmostBlock = targetBlocks[0];
+
+        let targetBlock = topmostBlock;
+        let insertedBlock = replaceWith;
+
+        //handle the case when there's a block connected to the block target
+        // let previousBlock = targetBlock.previousConnection.targetBlock();
+        let prevConn = targetBlock.previousConnection.targetConnection;
+        if (prevConn) {
+            prevConn.connect(insertedBlock.previousConnection);
+        } else {
+            // place inserted block close to the postion of the target block to avoid block jump
+            let xy = targetBlock.getRelativeToSurfaceXY();
+            insertedBlock.moveBy(xy.x, xy.y);
+        }
+
+        // unplug bottom block
+        let lastBlock = targetBlocks[targetBlocks.length - 1];
+
+        // the block after the replaced fragment
+        let nextBlock = lastBlock.nextConnection.targetBlock();
+        if (nextBlock) {
+            let nextConn = lastBlock.nextConnection.targetBlock().previousConnection;
+            lastBlock.unplug(); //must be unplugged before nextConn can connect to the inserted block
+            insertedBlock.nextConnection.connect(nextConn);
+        }
+
+        // dispose the fragment
+        while (targetBlocks.length > 0) {
+            var toDispose = targetBlocks.pop();
+            toDispose.dispose();
+        }
+
     }
-  
-  };
+
+};
